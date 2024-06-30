3f6f614359c884145ded00e6eab06b76aabd36da
OmerKenig <omerkenig@gmail.com>
2024-06-30 10:49:40 +0300

    Update for Azure_Module

---
+++ Azure_Module/modules/network.tf
@@ -0,0 +1,62 @@
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_machine_name
+  address_space = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  #   name                = "acceptanceTestPublicIp1-${count.index + 1}"
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+#   count               = var.NumberOfMachines
+}
+
+resource "azurerm_network_interface" "example" {
+#   name                = "example-nic-${count.index + 1}"
+    name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+#   count               = var.NumberOfMachines
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+#   network_interface_id      = azurerm_network_interface.example[count.index].id
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+#   count                     = var.NumberOfMachines
+}
